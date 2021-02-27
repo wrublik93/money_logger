cecca9496623255eb799e13757a4664c4eff39e2
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-28 00:50:15 +0300

    feat: implement query selector for form

--- public/app.js
+++ public/app.js
@@ -1,3 +1,10 @@
 "use strict";
-var anchor = document.querySelector('a');
-console.log(anchor);
+var form = document.querySelector('.new-item-form');
+var type = document.querySelector('#type');
+var toFrom = document.querySelector('#toFrom');
+var details = document.querySelector('#details');
+var amount = document.querySelector('#amount');
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+    console.log(type.value, toFrom.value, details.value, amount.valueAsNumber);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
-const anchor = document.querySelector('a');
+const form = document.querySelector('.new-item-form') as HTMLFormElement;
+const type = document.querySelector('#type') as HTMLSelectElement;
+const toFrom = document.querySelector('#toFrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLSelectElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;
 
-console.log(anchor);
+form.addEventListener('submit', (e: Event) => {
+    e.preventDefault();
+    console.log(
+        type.value,
+        toFrom.value,
+        details.value,
+        amount.valueAsNumber
+    )
+})
