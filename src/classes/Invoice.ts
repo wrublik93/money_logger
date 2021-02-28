cdbc9dd2dd789ede367f4d83ddb9a20bcc0af13e
Ivan Vrubleuski <wrublik.93@mail.ru>
2021-02-28 22:55:33 +0300

    feat: add class Payment

--- src/classes/Invoice.ts
+++ src/classes/Invoice.ts
@@ -1,4 +1,6 @@
-class Invoice {
+import { HasFormatter } from '../interfaces/HasFormatter.js';
+
+class Invoice implements HasFormatter {
     constructor(
         readonly client: string,
         private details: string,
